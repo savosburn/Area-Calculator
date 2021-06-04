bc3a20b4678bad6147e3eab4f59ca745eb95ecd9
savosburn <savosburn18@tampabay.rr.com>
2021-06-03 23:00:23 -0400

    9 hours and lots of procrastination later, we committed program 7

--- src/main/java/ex07/App.java
+++ src/main/java/ex07/App.java
@@ -5,8 +5,49 @@
 
 package ex07;
 
+import java.util.Scanner;
+
 public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static double CONVERSION = 0.09290304;
+
     public static void main(String[] args) {
-        
+        App myApp = new App();
+
+        int length = myApp.readLength();
+        int width = myApp.readWidth();
+
+        int area = myApp.areaFeet(length, width);
+        double areaMeters = myApp.areaMeters(area);
+
+        String generateOutput = myApp.concatStrings(length, width, area, areaMeters);
+        myApp.output(generateOutput);
+    }
+
+    public int readLength() {
+        System.out.print("What is the length of the room in feet? ");
+        return in.nextInt();
+    }
+
+    public int readWidth() {
+        System.out.print("What is the width of the room in feet? ");
+        return in.nextInt();
+    }
+
+    public int areaFeet(int length, int width) {
+        return length * width;
+    }
+
+    public double areaMeters(int area) {
+        return area * CONVERSION;
+    }
+
+    public String concatStrings(int length, int width, int area, double areaMeters){
+        return String.format("You entered dimensions of %d feet by %d feet.\nThe area is\n%d square feet\n%.3f square meters", length, width, area, areaMeters);
+    }
+
+    public void output(String printThis) {
+        System.out.println(printThis);
     }
 }
---
+++ src/test/java/ex07/AppTest.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
+package ex07;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    public void output_string_includes_area_and_converted_area() {
+        App myApp = new App();
+
+        int length = 15;
+        int width = 20;
+        int area = 15 * 20;
+        double areaConverted = area * 0.09290304;
+
+        String expectedOutput = """
+                You entered dimensions of 15 feet by 20 feet.
+                The area is
+                300 square feet
+                27.871 square meters""";
+
+        String actualOutput = myApp.concatStrings(length, width, area, areaConverted);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
